2ca2d47b23f620d388c14b97d80aff3f169a0e57
Erika Reiman <erika.reiman@gmail.com>
2019-11-16 19:38:10 -0500

    Added renderParagraph function

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -8,19 +8,26 @@ class Accordion extends React.Component {
         currentSection: null
     }
 
-    handleButtonClick () {
+    handleButtonClick (index) {
         console.log('button clicked!', {index});
+        this.setState({
+            currentSection: index
+        })
         
     }
+
+    renderParagraph(index) {
+        const paragraph = this.props.sections[index].content;
+        return (<p className="section">{paragraph}</p>)
+    }
     render() {
+        
         const accordion = this.props.sections.map((section, index) => (
            <li key={section.title}>
             <button key={index} onClick={() => this.handleButtonClick(index)}>
                     {section.title}
             </button>
-            <p className="section">
-                {section.content}
-            </p>
+           {this.renderParagraph(index)};
             </li>
         ))
         return (
